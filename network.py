8e24968a3f073f35ce2f6a9e5bd7b0605c030eb5
takyamamoto <41336057+takyamamoto@users.noreply.github.com>
2020-04-30 11:40:06 +0900

    Update network.py

--- network.py
+++ network.py
@@ -1,104 +1,79 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import scipy.optimize as opt
 
-
-class RaoBallard1999Model:
+class OlshausenField1996Model:
     def __init__(self, dt=1, sigma2=1, sigma2_td=10):
         self.dt = dt
         self.inv_sigma2 = 1/sigma2 # 1 / sigma^2        
-        self.inv_sigma2_td = 1/sigma2_td # 1 / sigma_td^2
         
-        self.k1 = 0.3 # k_1: update rate
-        self.k2 = 0.2 # k_2: learning rate
-        
-        self.lam = 0.02 # sparsity rate
-        self.alpha = 1
-        self.alphah = 0.05
+        self.eta = 1.0 # k_2: learning rate
+        self.alpha = 0.14 # lambda / sigma
         
         self.num_units_level0 = 256
-        self.num_units_level1 = 32
-        self.num_units_level2 = 128
-        self.num_level1 = 3
+        self.num_units_level1 = 100
+        self.batch_size = 100
         
         U = np.random.randn(self.num_units_level0, 
                             self.num_units_level1)
-        Uh = np.random.randn(int(self.num_level1*self.num_units_level1),
-                             self.num_units_level2)
         self.U = U.astype(np.float32) * np.sqrt(2/(self.num_units_level0+self.num_units_level1))
-        self.Uh = Uh.astype(np.float32) * np.sqrt(2/(int(self.num_level1*self.num_units_level1)+self.num_units_level2)) 
                 
-        self.r = np.zeros((self.num_level1, self.num_units_level1))
-        self.rh = np.zeros((self.num_units_level2))
+        self.r = np.zeros((self.batch_size, self.num_units_level1))
     
     def initialize_states(self, inputs):
-        self.r = np.array([self.U.T @ inputs[i] for i in range(self.num_level1)])
-        self.rh = self.Uh.T @ np.reshape(self.r, (int(self.num_level1*self.num_units_level1)))
+        self.r = np.array([self.U.T @ inputs[i] for i in range(self.batch_size)])
     
-    def calculate_total_error(self, error, errorh):
-        recon_error = self.inv_sigma2*np.sum(error**2) + self.inv_sigma2_td*np.sum(errorh**2)
-        sparsity_r = self.alpha*np.sum(self.r**2) + self.alphah*np.sum(self.rh**2)
-        sparsity_U = self.lam*(np.sum(self.U**2) + np.sum(self.Uh**2))
-        return recon_error + sparsity_r + sparsity_U
+    def normalize_rows(self):
+        self.U = 0.05*self.U / np.linalg.norm(self.U, ord=2, axis=1, keepdims=True)
+
+    # fun(x, *args) -> float
+    def _cost(self, r_, U_, input_):
+        error = input_ - U_ @ r_ 
+        
+        sigma = np.std(input_)
+        rs = r_ / sigma
+        S_r = self.alpha * sigma * np.sum(np.log(1 + rs**2))
+        return np.sum(error**2) + S_r
+
+    def _gradient(self, r_, U_, input_):
+        error = input_ - U_ @ r_ 
+
+        sigma = np.std(input_)
+        rs = r_ / sigma
+        g_r = self.alpha * rs / (1 + rs**2) 
+
+        return - U_.T @ error + g_r
+    
+    def _optimize_coefficients(self, U_, input_):
+        r_init = U_.T @ input_
+
+        res = opt.minimize(fun=self._cost, 
+                           x0=r_init,
+                           args=(U_, input_),
+                           method='CG',
+                           jac=self._gradient,
+                           options={'gtol': 1e-2, 'disp': False})
+        return res
+
+    def calculate_total_error(self, error):
+        recon_error = np.mean(error**2)
+        sparsity_r = self.alpha*np.mean(self.r**2) 
+        return recon_error + sparsity_r
         
     def __call__(self, inputs, training=False):
-        # inputs : (3, 256)
-        r_reshaped = np.reshape(self.r, (int(self.num_level1*self.num_units_level1))) # (96)
-
-        fx = np.array([self.U @ self.r[i] for i in range(self.num_level1)]) # (3, 256)
-        #fx = np.array([np.tanh(self.U @ self.r[i]) for i in range(self.num_level1)]) # (3, 256)
-
-        fxh = self.Uh @ self.rh # (96, )
-        #fxh = np.tanh(self.Uh @ self.rh) # (96, )
-        
-        #dfx = 1 - fx**2 # (3, 256)
-        #dfxh = 1 - fxh**2 # (96,)
-        
-        error = inputs - fx # (3, 256)
-        errorh = r_reshaped - fxh # (96, ) 
-        errorh_reshaped = np.reshape(errorh, (self.num_level1, self.num_units_level1)) # (3, 32)
-
-        #dfx_error = dfx * error # (3, 256)
-        #dfxh_errorh = dfxh * errorh # (96, )
-        
-        g_r = self.alpha * self.r / (1 + self.r**2) # (3, 32)
-        g_rh = self.alphah * self.rh / (1 + self.rh**2) # (64, )
-        #g_r = self.alpha * self.r  # (3, 32)
-        #g_rh = self.alphah * self.rh # (64, )
-
-        dr = self.inv_sigma2 * np.array([self.U.T @ error[i] for i in range(self.num_level1)])\
-            - self.inv_sigma2_td * errorh_reshaped - g_r
-        drh = self.inv_sigma2_td * self.Uh.T @ errorh - g_rh
-        
-        """
-        dr = self.inv_sigma2 * np.array([self.U.T @ dfx_error[i] for i in range(self.num_level1)])\
-            - self.inv_sigma2_td * errorh_reshaped - g_r
-        drh = self.inv_sigma2_td * self.Uh.T @ dfxh_errorh - g_rh
-        """
-        
-        dr = self.k1 * dr
-        drh = self.k1 * drh
-        
         # Updates                
-        self.r += dr
-        self.rh += drh
+        for i in range(self.batch_size):
+            self.r[i] = self._optimize_coefficients(self.U, inputs[i]).get('x')
+
+        fx = np.array([self.U @ self.r[i] for i in range(self.batch_size)]) # (3, 256)
+        error = inputs - fx # (3, 256)
         
         if training:  
-            """
-            dU = self.inv_sigma2 * np.sum(np.array([np.outer(dfx_error[i], self.r[i]) for i in range(self.num_level1)]),axis=0)\
-                - 3*self.lam * self.U
-            dUh = self.inv_sigma2_td * np.outer(dfxh_errorh, self.rh)\
-                - self.lam * self.Uh
-            """
-            dU = self.inv_sigma2 * np.sum(np.array([np.outer(error[i], self.r[i]) for i in range(self.num_level1)]), axis=0)\
-                - 3*self.lam * self.U
-            dUh = self.inv_sigma2_td * np.outer(errorh, self.rh)\
-                - self.lam * self.Uh
+            dU = np.mean(np.array([np.outer(error[i], self.r[i]) for i in range(self.batch_size)]), axis=0)            
+            self.U += self.eta * dU
             
-            self.U += self.k2 * dU
-            self.Uh += self.k2 * dUh
-            
-        return error, errorh, dr, drh
+        return error
             
             
             
