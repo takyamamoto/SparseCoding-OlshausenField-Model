63e22585f98b5d8883811237f22e2d2f237f07dd
takyamamoto <41336057+takyamamoto@users.noreply.github.com>
2020-05-01 04:16:22 +0900

    Update network.py

--- network.py
+++ network.py
@@ -1,79 +1,65 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import scipy.optimize as opt
 
 class OlshausenField1996Model:
-    def __init__(self, dt=1, sigma2=1, sigma2_td=10):
-        self.dt = dt
-        self.inv_sigma2 = 1/sigma2 # 1 / sigma^2        
+    def __init__(self, num_inputs, num_units, batch_size,
+                 lr_r=1e-2, lr_Phi=1e-2, lmda=1e-3):
+        self.lr_r = lr_r # learning rate of r
+        self.lr_Phi = lr_Phi # learning rate of Phi
+        self.lmda = lmda # regularization parameter
         
-        self.eta = 1.0 # k_2: learning rate
-        self.alpha = 0.14 # lambda / sigma
+        self.num_inputs = num_inputs
+        self.num_units = num_units
+        self.batch_size = batch_size
         
-        self.num_units_level0 = 256
-        self.num_units_level1 = 100
-        self.batch_size = 100
-        
-        U = np.random.randn(self.num_units_level0, 
-                            self.num_units_level1)
-        self.U = U.astype(np.float32) * np.sqrt(2/(self.num_units_level0+self.num_units_level1))
-                
-        self.r = np.zeros((self.batch_size, self.num_units_level1))
+        # Weights
+        Phi = np.random.randn(self.num_inputs, self.num_units).astype(np.float32)
+        self.Phi = Phi * np.sqrt(1/self.num_units)
+
+        # activity of neurons
+        self.r = np.zeros((self.batch_size, self.num_units))
     
-    def initialize_states(self, inputs):
-        self.r = np.array([self.U.T @ inputs[i] for i in range(self.batch_size)])
-    
+    def initialize_states(self):
+        self.r = np.zeros((self.batch_size, self.num_units))
+        
     def normalize_rows(self):
-        self.U = 0.05*self.U / np.linalg.norm(self.U, ord=2, axis=1, keepdims=True)
+        self.Phi = self.Phi / np.maximum(np.linalg.norm(self.Phi, ord=2, axis=0, keepdims=True), 1e-8)
 
-    # fun(x, *args) -> float
-    def _cost(self, r_, U_, input_):
-        error = input_ - U_ @ r_ 
-        
-        sigma = np.std(input_)
-        rs = r_ / sigma
-        S_r = self.alpha * sigma * np.sum(np.log(1 + rs**2))
-        return np.sum(error**2) + S_r
+    # thresholding function of S(x)=|x|
+    def soft_thresholding_func(self, x, lmda):
+        return np.maximum(x - lmda, 0) - np.maximum(-x - lmda, 0)
 
-    def _gradient(self, r_, U_, input_):
-        error = input_ - U_ @ r_ 
+    # thresholding function of S(x)=ln(1+x^2)
+    def ln_thresholding_func(self, x, lmda):
+        f = 9*lmda*x - 2*np.power(x, 3) - 18*x
+        g = 3*lmda - np.square(x) + 3
+        h = np.cbrt(np.sqrt(np.square(f) + 4*np.power(g, 3)) + f)
+        two_croot = np.cbrt(2) # cubic root of two
+        return (1/3)*(x - h / two_croot + two_croot*g / (1e-8+h))
 
-        sigma = np.std(input_)
-        rs = r_ / sigma
-        g_r = self.alpha * rs / (1 + rs**2) 
-
-        return - U_.T @ error + g_r
-    
-    def _optimize_coefficients(self, U_, input_):
-        r_init = U_.T @ input_
-
-        res = opt.minimize(fun=self._cost, 
-                           x0=r_init,
-                           args=(U_, input_),
-                           method='CG',
-                           jac=self._gradient,
-                           options={'gtol': 1e-2, 'disp': False})
-        return res
+    # thresholding function https://arxiv.org/abs/2003.12507
+    def cauchy_thresholding_func(self, x, lmda):
+        f = 0.5*(x + np.sqrt(np.maximum(x**2 - lmda,0)))
+        g = 0.5*(x - np.sqrt(np.maximum(x**2 - lmda,0)))
+        return f*(x>=lmda) + g*(x<=-lmda) 
 
     def calculate_total_error(self, error):
         recon_error = np.mean(error**2)
-        sparsity_r = self.alpha*np.mean(self.r**2) 
+        sparsity_r = self.lmda*np.mean(np.abs(self.r)) 
         return recon_error + sparsity_r
         
-    def __call__(self, inputs, training=False):
+    def __call__(self, inputs, training=True):
         # Updates                
-        for i in range(self.batch_size):
-            self.r[i] = self._optimize_coefficients(self.U, inputs[i]).get('x')
-
-        fx = np.array([self.U @ self.r[i] for i in range(self.batch_size)]) # (3, 256)
-        error = inputs - fx # (3, 256)
+        error = inputs - self.r @ self.Phi.T
+        
+        r = self.r + self.lr_r * error @ self.Phi
+        self.r = self.soft_thresholding_func(r, self.lmda)
+        #self.r = self.cauchy_thresholding_func(r, self.lmda)
         
         if training:  
-            dU = np.mean(np.array([np.outer(error[i], self.r[i]) for i in range(self.batch_size)]), axis=0)            
-            self.U += self.eta * dU
+            error = inputs - self.r @ self.Phi.T
+            dPhi = error.T @ self.r
+            self.Phi += self.lr_Phi * dPhi
             
-        return error
-            
-            
-            
+        return error, self.r
